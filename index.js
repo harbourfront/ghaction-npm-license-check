29c41daa7c566b338a22670162bcb63d9c8d2f4b
Dylan Kok <me@dylankjy.com>
2022-10-12 03:16:03 +0800

    Commit code

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core")
+const checker = require("license-checker-rseidelsohn")
+const excludePrivatePackages = core.getInput("exclude-private-packages") === "true"
+const productionOnly = core.getInput("production-only") === "true"
+
+try {
+	checker.init({
+		start: '/codebase',
+		excludePrivatePackages,
+		production: productionOnly,
+		summary: true
+	}, (err, packages) => {
+		if (err) {
+			core.error("Dependency license check failed.");
+			core.setFailed(err);
+			return;
+		}
+		core.info("Dependency license all passing.");
+	});
+}
+catch(err) {
+	core.setFailed(err.message);
+}
